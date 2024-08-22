5ad7fec7b3c92694a4b2b7ce3563cedb03f662b3
Cedric <young.cet@gmail.com>
2024-08-22 23:46:43 +0200

    updated demo

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,9 +19,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "398319c24c5b28660a9b7e4ea6d39ca3",
-"/": "398319c24c5b28660a9b7e4ea6d39ca3",
-"main.dart.js": "f4d930c6e4f1dcaaad525c385c21e9fb",
+"index.html": "6700ba1bba07a46846061bdc7dcee014",
+"/": "6700ba1bba07a46846061bdc7dcee014",
+"main.dart.js": "dd79001cf2b5bcac10b373af6316380f",
 "manifest.json": "9a0de1ff26661cd281cb9deb3db33b02",
 "version.json": "ff966ab969ba381b900e61629bfb9789"
 };
